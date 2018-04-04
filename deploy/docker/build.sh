ec2ddc1c54800a8eaad158b34864d5be57763618
Michael Johnson <mj82@grnoc.iu.edu>
2018-04-04 20:36:16 +0000

    Remove config.js from the docker image #38

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -4,6 +4,8 @@ echo "preparing mca-admin"
 rm -rf mca-admin/tmp
 mkdir mca-admin/tmp
 cp -r ../../api mca-admin/tmp
+rm -f mca-admin/tmp/api/config.js
+#ls -la mca-admin/tmp/api/config.js
 cp -r ../../ui mca-admin/tmp
 cp -r ../../package.json mca-admin/tmp
 rm -rf mca-admin/tmp/api/config
@@ -21,6 +23,7 @@ echo "preparing mca-pub"
 rm -rf mca-pub/tmp
 mkdir mca-pub/tmp
 cp -r ../../api mca-pub/tmp
+rm -f mca-admin/tmp/api/config.js
 cp -r ../../package.json mca-pub/tmp
 rm -rf mca-pub/tmp/api/config
 
